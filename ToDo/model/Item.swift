725ce83f4ce4803fafeed4c96e14b475398e5088
Billidevelopers <billidevlopers@gmail.com>
2020-08-01 14:25:49 +0530

    sucessfully adding task with codable property file

---
+++ ToDo/model/Item.swift
@@ -0,0 +1,17 @@
+//
+//  Item.swift
+//  ToDo
+//
+//  Created by Ravi Thakur on 31/07/20.
+//  Copyright © 2020 billidevelopers. All rights reserved.
+//
+
+import Foundation
+
+struct Item : Codable{
+    var title = String()
+    var cheacked = Bool()
+    
+    
+    
+}
